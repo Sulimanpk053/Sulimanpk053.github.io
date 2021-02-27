b29610ad13081aeadff7aa51b3f2e1d3dcc4e572
sulimanpk053 <sulimanpk053@gmail.com>
2021-02-27 15:31:55 +0100

    Intitial Commit

--- SmartCity-AR/js/multi-location.js
+++ SmartCity-AR/js/multi-location.js
@@ -23,21 +23,21 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        scale: '1 1 1',
         info: 'Magnemite, Lv. 5, HP 1/1',
         rotation: '0 0 0',
     },
     {
         url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
+        scale: '1 1 1',
         rotation: '0 0 0',
-        info: 'Articuno, Lv. 80, HP 1/1',
+        info: 'Articuno, Lv. 8, HP 1/1',
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
+        scale: '1 1 1',
         rotation: '0 0 0',
-        info: 'Dragonite, Lv. 99, HP 1/1',
+        info: 'Dragonite, Lv. 9, HP 1/1',
     },
 ];
 
